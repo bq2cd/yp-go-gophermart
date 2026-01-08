d8253d35a7831c4be31b048dbd975ceba49f6fbf
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-01-08 18:21:00 +0300

    cmd: add comment to satisfy 'revive' linter

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,5 @@
+// Binary gophermart provides a CLI entry point to launch GopherMart HTTP API and
+// corresponding processing sub-systems, e.g. order processing, etc.
 package main
 
 func main() {}
