7b249565a3dd06340e250383982a3e97af20244e
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-01-09 19:53:33 +0300

    handler: complete 'get balance' test scenarios

--- internal/gophermart/handler/h_get_balance_test.go
+++ internal/gophermart/handler/h_get_balance_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"errors"
 	"net/http"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -14,6 +15,7 @@ var _ = Describe("GetBalance", func() {
 	var (
 		testCtx   *TestContext
 		testMocks *TestMocks
+		userLogin string
 	)
 
 	BeforeEach(func() {
@@ -30,7 +32,6 @@ var _ = Describe("GetBalance", func() {
 
 	When("user is authenticated", func() {
 		var (
-			userLogin       string
 			authToken       string
 			expectedBalance api.Balance
 		)
@@ -62,4 +63,64 @@ var _ = Describe("GetBalance", func() {
 		})
 	})
 
+	DescribeTableSubtree("user provided incorrect token",
+		func(authHeaderValue string, setupMocks func()) {
+			BeforeEach(func() {
+				userLogin = "user2"
+
+				testCtx.Request.Header.Set(api.AuthorizationHeaderName, authHeaderValue)
+
+				setupMocks()
+			})
+
+			It("should return 401 Unauthorized", func() {
+				Expect(testCtx.GetStatusCode()).To(Equal(http.StatusUnauthorized))
+				Expect(testCtx.GetBodyBytes()).To(BeEmpty())
+			})
+		},
+		Entry("random token", "Bearer some-random-string", func() {
+			testMocks.TokenService.EXPECT().
+				ValidateToken(domain.Token("some-random-string")).
+				Return(domain.UserID(userLogin), domain.ErrUserAuthenticationFailed)
+		}),
+		Entry("correct token without Bearer prefix", "some-valid-token", func() {}),
+	)
+
+	DescribeTableSubtree("internal error happens",
+		func(setupMocks func()) {
+			var (
+				authToken string
+			)
+			BeforeEach(func() {
+				userLogin = "user3"
+				authToken = "valid-token3"
+
+				testCtx.Request.SetAuthToken(authToken)
+
+				testMocks.TokenService.EXPECT().
+					ValidateToken(domain.Token(authToken)).
+					Return(domain.UserID(userLogin), nil)
+
+				setupMocks()
+			})
+
+			It("should return 500 Internal Server Error", func() {
+				Expect(testCtx.GetStatusCode()).To(Equal(http.StatusInternalServerError))
+				Expect(testCtx.GetBodyBytes()).To(BeEmpty())
+			})
+		},
+		Entry("when getting user's balance current value", func() {
+			testMocks.BalanceService.EXPECT().
+				GetBalance(domain.UserID(userLogin)).
+				Return(0, errors.New("balance current value error"))
+		}),
+		Entry("when getting user's total withdrawn amount", func() {
+			testMocks.BalanceService.EXPECT().
+				GetBalance(domain.UserID(userLogin)).
+				Return(3.5, nil)
+			testMocks.BalanceService.EXPECT().
+				GetTotalAmountWithdrawn(domain.UserID(userLogin)).
+				Return(0, errors.New("withdrawn amount error"))
+		}),
+	)
 })
