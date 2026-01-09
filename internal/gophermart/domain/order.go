1e5fc9054f2a4e1d9546d730734eda8289ce50fa
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-01-09 20:40:47 +0300

    domain: add order-related types

---
+++ internal/gophermart/domain/order.go
@@ -0,0 +1,27 @@
+package domain
+
+// OrderID represents the ID of an [Order].
+type OrderID uint64
+
+// OrderStatus represent the status of an [Order].
+type OrderStatus int
+
+const (
+	// OrderStatusInvalid is assigned to an [Order] when it fails business validation.
+	// This is a final status.
+	OrderStatusInvalid OrderStatus = iota - 1
+	// OrderStatusNew is assigned to newly created [Order].
+	OrderStatusNew
+	// OrderStatusProcessing is assigned to an [Order] that has been put into an internal processing pipeline.
+	OrderStatusProcessing
+	// OrderStatusProcessed is assigned to an [Order] when it has been fully processed.
+	// This is a final status.
+	OrderStatusProcessed
+)
+
+// Order combines essential information about a user's order in the system.
+type Order struct {
+	ID     OrderID
+	Status OrderStatus
+	User   UserID
+}
--- internal/gophermart/domain/withdrawal.go
+++ internal/gophermart/domain/withdrawal.go
@@ -5,6 +5,7 @@ import "time"
 // WithdrawalTransaction describes an action of using funds on user's balance
 // to pay for a new order.
 type WithdrawalTransaction struct {
+	OrderID     OrderID
 	Amount      float64
 	ProcessedAt time.Time
 }
