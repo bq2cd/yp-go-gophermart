fe9df58df1b7f94c022764bfaa5423d6906c6cd2
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-01-09 20:41:08 +0300

    handler: implementation + tests for 'list withdrawals' operation

--- internal/gophermart/handler/h_list_withdrawals.go
+++ internal/gophermart/handler/h_list_withdrawals.go
@@ -1,8 +1,51 @@
 package handler
 
-import "github.com/bq2cd/yp-go-gophermart/internal/gophermart/handler/api"
+import (
+	"strconv"
+
+	"github.com/bq2cd/yp-go-gophermart/internal/gophermart/domain"
+	"github.com/bq2cd/yp-go-gophermart/internal/gophermart/handler/api"
+)
 
 // ListWithdrawals implements [api.OperationListWithdrawals].
 func (h *Handler) ListWithdrawals(operation *api.OperationListWithdrawals) {
+	userID, ok := h.ensureUserID(operation)
+	if !ok {
+		return
+	}
+
+	withdrawals, err := h.balanceService.GetWithdrawalTransactions(userID)
+	if err != nil {
+		h.sendListWithdrawalsError(operation)
+
+		return
+	}
+
+	h.sendListWithdrawalsOK(operation, withdrawals)
+}
+
+func (h *Handler) sendListWithdrawalsError(operation *api.OperationListWithdrawals) {
 	operation.RespondServerError()
+}
+
+func (h *Handler) sendListWithdrawalsOK(
+	operation *api.OperationListWithdrawals,
+	withdrawals []domain.WithdrawalTransaction,
+) {
+	if len(withdrawals) == 0 {
+		operation.RespondNoContent()
+
+		return
+	}
+
+	apiWithdrawals := make([]api.WithdrawalTransaction, 0, len(withdrawals))
+	for _, item := range withdrawals {
+		apiWithdrawals = append(apiWithdrawals, api.WithdrawalTransaction{
+			Order:       strconv.FormatUint(uint64(item.OrderID), 10),
+			ProcessedAt: item.ProcessedAt,
+			Sum:         item.Amount,
+		})
+	}
+
+	operation.RespondOK(apiWithdrawals)
 }
---
+++ internal/gophermart/handler/h_list_withdrawals_test.go
@@ -0,0 +1,149 @@
+package handler_test
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/bq2cd/yp-go-gophermart/internal/gophermart/domain"
+	"github.com/bq2cd/yp-go-gophermart/internal/gophermart/handler/api"
+)
+
+var _ = Describe("ListWithdrawals", func() {
+	var (
+		testCtx   *TestContext
+		testMocks *TestMocks
+		userLogin string
+	)
+
+	BeforeEach(func() {
+		testCtx = InitTestContext()
+		testMocks = InitTestMocks(testCtx)
+
+		testCtx.Request.Method = http.MethodGet
+		testCtx.Request.Path = "/api/user/withdrawals"
+	})
+
+	JustBeforeEach(func() {
+		testCtx.ProcessRequest()
+	})
+
+	Context("user is authenticated", func() {
+		var (
+			authToken string
+		)
+
+		BeforeEach(func() {
+			userLogin = "user1"
+			authToken = "some-valid-token"
+
+			testCtx.Request.SetAuthToken(authToken)
+
+			testMocks.TokenService.EXPECT().
+				ValidateToken(domain.Token(authToken)).
+				Return(domain.UserID(userLogin), nil)
+		})
+
+		When("user has previously made some withdrawals", func() {
+			var (
+				now                 time.Time
+				expectedWithdrawals []api.WithdrawalTransaction
+			)
+
+			BeforeEach(func() {
+				now = time.Now()
+				expectedWithdrawals = []api.WithdrawalTransaction{
+					{
+						Order:       "12345",
+						ProcessedAt: now.UTC(),
+						Sum:         3.15,
+					},
+					{
+						Order:       "67890",
+						ProcessedAt: now.Add(-1 * time.Hour).UTC(),
+						Sum:         7.31,
+					},
+				}
+
+				testMocks.BalanceService.EXPECT().
+					GetWithdrawalTransactions(domain.UserID(userLogin)).
+					Return([]domain.WithdrawalTransaction{
+						{
+							OrderID:     domain.OrderID(12345),
+							Amount:      3.15,
+							ProcessedAt: now.UTC(),
+						},
+						{
+							OrderID:     domain.OrderID(67890),
+							Amount:      7.31,
+							ProcessedAt: now.Add(-1 * time.Hour).UTC(),
+						},
+					}, nil)
+			})
+
+			It("should return 200 OK and JSON-encoded array of withdrawal transactions", func() {
+				Expect(testCtx.GetStatusCode()).To(Equal(http.StatusOK))
+				Expect(
+					UnmashalBodyJSON[[]api.WithdrawalTransaction](testCtx.GetBodyBytes()),
+				).To(Equal(expectedWithdrawals))
+			})
+		})
+
+		When("user has never made any withdrawals", func() {
+			BeforeEach(func() {
+				testMocks.BalanceService.EXPECT().
+					GetWithdrawalTransactions(domain.UserID(userLogin)).
+					Return(nil, nil)
+			})
+
+			It("should return 204 No Content", func() {
+				Expect(testCtx.GetStatusCode()).To(Equal(http.StatusNoContent))
+				Expect(testCtx.GetBodyBytes()).To(BeEmpty())
+			})
+		})
+
+		DescribeTableSubtree("internal error happens",
+			func(setupMocks func()) {
+				BeforeEach(func() {
+					setupMocks()
+				})
+
+				It("should return 500 Internal Server Error", func() {
+					Expect(testCtx.GetStatusCode()).To(Equal(http.StatusInternalServerError))
+					Expect(testCtx.GetBodyBytes()).To(BeEmpty())
+				})
+			},
+			Entry("when getting withdrawal transactions", func() {
+				testMocks.BalanceService.EXPECT().
+					GetWithdrawalTransactions(domain.UserID(userLogin)).
+					Return(nil, errors.New("error getting transactions"))
+			}),
+		)
+	})
+
+	DescribeTableSubtree("user provided incorrect token",
+		func(authHeaderValue string, setupMocks func()) {
+			BeforeEach(func() {
+				userLogin = "user3"
+
+				testCtx.Request.Header.Set(api.AuthorizationHeaderName, authHeaderValue)
+
+				setupMocks()
+			})
+
+			It("should return 401 Unauthorized", func() {
+				Expect(testCtx.GetStatusCode()).To(Equal(http.StatusUnauthorized))
+				Expect(testCtx.GetBodyBytes()).To(BeEmpty())
+			})
+		},
+		Entry("random token", "Bearer some-random-string", func() {
+			testMocks.TokenService.EXPECT().
+				ValidateToken(domain.Token("some-random-string")).
+				Return(domain.UserID(userLogin), domain.ErrUserAuthenticationFailed)
+		}),
+		Entry("correct token without Bearer prefix", "some-valid-token", func() {}),
+	)
+})
