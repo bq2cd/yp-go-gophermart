a0512a9c70387543d0e5dcffd34dc93086ac3e40
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-01-18 23:41:17 +0300

    testutil: helpers for getting caller function name

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -3,8 +3,15 @@ package testutil
 import (
 	"context"
 	"reflect"
+	"runtime"
+	"strings"
 
 	"go.uber.org/mock/gomock"
+)
+
+const (
+	callerFrameMaxPC    = 16
+	callerFrameBaseSkip = 2
 )
 
 // MockCtx is designed to match [context.Context] arguments
@@ -15,4 +22,22 @@ func MockCtx() gomock.Matcher {
 	ctx := reflect.TypeFor[context.Context]()
 
 	return gomock.AssignableToTypeOf(ctx)
+}
+
+// CallerFrame will return [runtime.Frame] for its caller or
+// a corresponding parent caller if [skip] is greater than zero.
+func CallerFrame(skip int) runtime.Frame {
+	pc := make([]uintptr, callerFrameMaxPC)
+	n := runtime.Callers(skip+callerFrameBaseSkip, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	first, _ := frames.Next()
+
+	return first
+}
+
+// FunctionBasename returns the last portion after a dot of the fully-qualified function name.
+func FunctionBasename(frame runtime.Frame) string {
+	parts := strings.Split(frame.Function, ".")
+
+	return parts[len(parts)-1]
 }
