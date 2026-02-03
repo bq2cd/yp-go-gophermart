a8d435600e611c1953e4c17c944a218719856b24
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-02-03 20:22:53 +0300

    [PR#1] test: count calls to fake accrual client and support custom errors
    There is a case where we need to return a custom error when querying
    our fake accrual client: namely, when we need to simulate the accrual
    server returning `429 Too Many Requests`.
    Also, counting a number of calls to the fake client would allow us to
    perform at least some verification of a custom retry logic implemented
    on a higher level.

--- internal/test/fakes/gophermart/service/workers/error_chain.go
+++ internal/test/fakes/gophermart/service/workers/error_chain.go
@@ -2,49 +2,108 @@
 package fakes
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 )
 
-type TestError struct {
-	chain *[]bool
+type testErrorCount struct {
+	err   error
+	count uint
 }
 
-func NewTestError(counts ...uint) TestError {
-	chain := make([]bool, 0)
+type testErrorChain struct {
+	mu    sync.RWMutex
+	chain []*testErrorCount
+}
 
-	for _, count := range counts {
-		if count == 0 {
-			chain = append(chain, false)
+func (tc *testErrorChain) calculateRemaining() uint {
+	tc.mu.RLock()
+	defer tc.mu.RUnlock()
 
-			continue
-		}
-
-		for range count {
-			chain = append(chain, true)
+	var remaining uint
+	for _, errCount := range tc.chain {
+		if errCount == nil {
+			remaining++
+		} else {
+			remaining += errCount.count
 		}
 	}
 
+	return remaining
+}
+
+func (tc *testErrorChain) getNextErrorCount() *testErrorCount {
+	tc.mu.Lock()
+	defer tc.mu.Unlock()
+
+	for len(tc.chain) > 0 {
+		errCount := tc.chain[0]
+
+		if errCount == nil || errCount.count == 0 {
+			tc.chain = tc.chain[1:]
+		}
+
+		if errCount == nil {
+			return nil
+		}
+
+		if errCount.count == 0 {
+			continue
+		}
+
+		errCount.count--
+
+		return errCount
+	}
+
+	return nil
+}
+
+type TestError struct {
+	*testErrorChain
+}
+
+func NewTestError(counts ...uint) TestError {
+	return NewTestErrorCustom(nil, counts...)
+}
+
+func NewTestErrorCustom(custom error, counts ...uint) TestError {
+	chain := make([]*testErrorCount, 0)
+
+	err := custom
+	if err == nil {
+		err = errors.New("test error")
+	}
+
+	for _, count := range counts {
+		var errCount *testErrorCount
+		if count > 0 {
+			errCount = &testErrorCount{err: err, count: count}
+		}
+
+		chain = append(chain, errCount)
+	}
+
 	return TestError{
-		chain: &chain,
+		testErrorChain: &testErrorChain{
+			mu:    sync.RWMutex{},
+			chain: chain,
+		},
 	}
 }
 
 func (te TestError) Next() error {
-	if te.chain == nil {
+	if te.testErrorChain == nil {
 		return nil
 	}
 
-	remaining := len(*te.chain)
-	if remaining == 0 {
+	remaining := te.calculateRemaining()
+
+	errCount := te.getNextErrorCount()
+	if errCount == nil {
 		return nil
 	}
 
-	wantErr := (*te.chain)[0]
-	*te.chain = (*te.chain)[1:]
-
-	if wantErr {
-		return fmt.Errorf("test error %d", remaining)
-	}
-
-	return nil
+	return fmt.Errorf("%w %d", errCount.err, remaining)
 }
